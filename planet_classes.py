4a1328d02e27a0a2e9f2cf6421b0f55f3bd30789
olivia <okrysiak@ucvts.tec.nj.us>
2018-01-22 23:04:47 +0000

    Planet class almost done

--- planet_classes.py
+++ planet_classes.py
@@ -8,28 +8,35 @@ class Planet(object):
 
     """
 
-    def __init__(self, name, temperature, moons, inhabited, population):
+    def __init__(self, name, temperature, moons, inhabited, population, description):
         """Return a new Planet object"""
         self.name = name
         self.temperature = temperature
         self.moons = moons
         self.inhabited = inhabited
         self.population = population
+        self.description = description
 
     def __str__(self):
-        return f""""{self.name} Status :
-    Temperature: {self.temperature} degrees Fahrenheit
+        return f"""{self.name} Status :
+    Current Temperature: {self.temperature} degrees Fahrenheit
     Number of Moons : {self.moons}
     Is it Inhabited? : {self.inhabited}
     Population: {self.population}"""
 
-mercury = Planet("Mercury", 100, 3, "yes", 300)
+mercury = Planet("Mercury", 657, 0, "No", 0)
+venus = Planet("Venus", 801, 0, "Yes", "approx. 2 billion")
+earth = Planet("Earth", 900, 1, "No", 0)
+mars = Planet("Mars", 70, 2, "Yes", "approx. 1 billion")
+jupiter = Planet("Jupiter",235, 67, "Yes", 3)
+saturn = Planet("Saturn", -288, 62, "Yes", "approx. 300 billion")
+uranus = Planet("Uranus", -357, 27, "No", 0)
 neptune = Planet("Neptune", -200, 2, "no", 0)
-# mercury = Planet("Mercury")
+pluto = Planet("Pluto", -380, 5, "Yes", "approx. 100,000")
 
-planets = [mercury, neptune]
+planets = [mercury,venus, earth, mars, jupiter, saturn, uranus, neptune, pluto ]
 
-current_planet = planets[0]
+current_planet = planets[4]
 
 #print(f"Mercury's temperature is {current_planet.temperature}")
-print(mercury)
+print(current_planet)
